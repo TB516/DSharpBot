6f86fd23a0d562927900daed352d74db80239deb
Thomas Berrios <tberrios9800@gmail.com>
2021-11-12 18:51:33 -0500

    Created first code (lol)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
 
 namespace TranscriptMakerBot{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync(){
+            var discord = new DiscordClient(new DiscordConfiguration(){
+                Token = "ODg3ODA5MjM3MDA5NDQwODAw.YUJiyA.w - oJYAb9TVhGNzPmwrCEsY5eqmo",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, e) =>{
+                if(e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync(discord.Ping.ToString());
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Discord_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
