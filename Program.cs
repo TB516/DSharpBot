c076a974f9a016378cd863dcb3e5056b20b7bfab
Thomas Berrios <tberrios9800@gmail.com>
2021-11-13 20:45:45 -0500

    Sam tought me about task, async and await and made me change my curly brackets.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,42 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using System;
 using System.Threading.Tasks;
 
-namespace TranscriptMakerBot{
-    class Program{
-        static void Main(string[] args){
+namespace TranscriptMakerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             MainAsync().GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync(){
-            var discord = new DiscordClient(new DiscordConfiguration(){
+        static async Task MainAsync()
+        {
+            DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+            {
                 Token = "ODg3ODA5MjM3MDA5NDQwODAw.YUJiyA.w - oJYAb9TVhGNzPmwrCEsY5eqmo",
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
-
-            discord.MessageCreated += async (s, e) =>{
-                if(e.Message.Content.ToLower().StartsWith("ping"))
-                    await e.Message.RespondAsync($"Your ping is {discord.Ping.ToString()}.");
+           
+            discord.MessageCreated += async (sender, e) =>{
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                {
+                    e.Message.RespondAsync($"Your ping is {discord.Ping} ms.");
+                    
+                }
+                    
+                    
             };
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
 
-        private static Task Discord_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e){
+        private static Task Discord_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
             throw new NotImplementedException();
         }
     }
