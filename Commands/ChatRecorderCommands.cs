36ef698fe64222803b4252dfefc89ff774d32d21
Thomas Berrios <tberrios9800@gmail.com>
2021-11-14 21:55:26 -0500

    Literally nothing changed

--- Commands/ChatRecorderCommands.cs
+++ Commands/ChatRecorderCommands.cs
@@ -7,14 +7,7 @@ class ChatRecorderCommands : BaseCommandModule
     [Command("startRecording")]
     public async Task RecordChannel(CommandContext ctx, [RemainingText] string topic)
     {
-        if (topic != null)
-        {
-            ctx.RespondAsync($"Started recording this chat! \nThis recording is about {topic}.");
-        }
-        else
-        {
-            ctx.RespondAsync("Started recording this chat! \nThe topic of this recording wasn't specified.");
-        }
+        ctx.RespondAsync($"Started recording this chat! \nThis recording is about {topic}.");
 
 
     }
