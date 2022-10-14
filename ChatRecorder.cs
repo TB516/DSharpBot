a6d19cb7b6fe4b43864ae3e6067ed4e9c5e5b56d
Thomas <tberrios9800@gmail.com>
2022-10-14 09:37:27 -0400

    Fixed naming of properties in chat recorder

--- ChatRecorder.cs
+++ ChatRecorder.cs
@@ -10,36 +10,36 @@ namespace DSharpBot
 {
     class ChatRecorder : IDisposable
     {
-        public static List<ChatRecorder> chatRecorders = new List<ChatRecorder>();
-        public DiscordChannel channel { get; set; }
-        public string conversationTopic { get; set; }
-        private string transcriptFilePath { get; set; }
-        private StreamWriter transcriptFileWriter { get; set; }
+        public static List<ChatRecorder> ChatRecorders = new List<ChatRecorder>();
+        public DiscordChannel Channel { get; set; }
+        public string ConversationTopic { get; set; }
+        private string TranscriptFilePath { get; set; }
+        private StreamWriter TranscriptFileWriter { get; set; }
 
         public ChatRecorder(DiscordChannel _channel, string _conversationTopic)
         {
-            channel = _channel;
-            conversationTopic = _conversationTopic;
+            Channel = _channel;
+            ConversationTopic = _conversationTopic;
 
             Directory.CreateDirectory(@$"{Directory.GetCurrentDirectory()}\Records");
-            transcriptFilePath = @$"{Directory.GetCurrentDirectory()}\Records\{conversationTopic}_{channel.Id}.txt";
-            transcriptFileWriter = new StreamWriter(transcriptFilePath);
+            TranscriptFilePath = @$"{Directory.GetCurrentDirectory()}\Records\{ConversationTopic}_{Channel.Id}.txt";
+            TranscriptFileWriter = new StreamWriter(TranscriptFilePath);
             Program.discord.MessageCreated += WriteToTextFile;
         }
         private async Task WriteToTextFile(DiscordClient sender, MessageCreateEventArgs e)
         {
-            if (e.Message.Channel == channel && !e.Message.Author.IsBot)
+            if (e.Message.Channel == Channel && !e.Message.Author.IsBot)
             {
                 DiscordMember messageAuthorMember = await e.Guild.GetMemberAsync(e.Author.Id);
-                transcriptFileWriter.WriteLine($"<{messageAuthorMember.DisplayName}> {e.Message.Content}");
+                TranscriptFileWriter.WriteLine($"<{messageAuthorMember.DisplayName}> {e.Message.Content}");
             }
         }
         public void Dispose()
         {
             Program.discord.MessageCreated -= WriteToTextFile;
-            transcriptFileWriter.Flush();
-            transcriptFileWriter.Close();
-            transcriptFileWriter.Dispose();
+            TranscriptFileWriter.Flush();
+            TranscriptFileWriter.Close();
+            TranscriptFileWriter.Dispose();
         }
     }
 }
--- Commands/ChatRecorderCommands.cs
+++ Commands/ChatRecorderCommands.cs
@@ -9,27 +9,27 @@ namespace DSharpBot
         [SlashCommand("startRec", "Starts recording the channel it is called in.")]
         public async Task RecordChannel(InteractionContext ctx, [Option("Topic", "The topic of the conversation being recorded.")] string conversationTopic)
         {   
-            for (int i = 0; i < ChatRecorder.chatRecorders.Count; i++)
+            for (int i = 0; i < ChatRecorder.ChatRecorders.Count; i++)
             {
-                if(ChatRecorder.chatRecorders[i].conversationTopic == conversationTopic)
+                if(ChatRecorder.ChatRecorders[i].ConversationTopic == conversationTopic)
                 {
                     ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder().WithContent($"Recording with the topic \"{conversationTopic}\" is already running"));
                     return;
                 }    
             }
-            ChatRecorder.chatRecorders.Add(new ChatRecorder(ctx.Channel, conversationTopic));
+            ChatRecorder.ChatRecorders.Add(new ChatRecorder(ctx.Channel, conversationTopic));
             ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder().WithContent($"Started recording this chat! \nThis recording is about \"{conversationTopic}\"."));
         }
         
         [SlashCommand("stopRec", "Ends the recording of the specified topic in the channel called in.")]
         public async Task EndRecording(InteractionContext ctx, [Option("Topic", "The topic of the conversation being recorded.")] string conversationTopic)
         {
-            for(int i = 0; i < ChatRecorder.chatRecorders.Count; i++)
+            for(int i = 0; i < ChatRecorder.ChatRecorders.Count; i++)
             {
-                if(ChatRecorder.chatRecorders[i].channel == ctx.Channel && ChatRecorder.chatRecorders[i].conversationTopic == conversationTopic)
+                if(ChatRecorder.ChatRecorders[i].Channel == ctx.Channel && ChatRecorder.ChatRecorders[i].ConversationTopic == conversationTopic)
                 {
-                    ChatRecorder.chatRecorders[i].Dispose();
-                    ChatRecorder.chatRecorders.RemoveAt(i);
+                    ChatRecorder.ChatRecorders[i].Dispose();
+                    ChatRecorder.ChatRecorders.RemoveAt(i);
                     ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder().WithContent($"Recording about \"{conversationTopic}\" ended!"));
                     return;
                 }
