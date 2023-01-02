c49c2f09640fdfb016b1c6ca66d0545ecc2be1a0
Thomas <tberrios9800@gmail.com>
2023-01-02 02:38:13 -0500

    Added additional checks to music player commands, started work on methods to play music, and added auto disconnect event to music player

--- Music Player/MusicPlayer.cs
+++ Music Player/MusicPlayer.cs
@@ -1,7 +1,9 @@
-﻿using DSharpBot.Music_Player;
+﻿using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.VoiceNext;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -15,14 +17,22 @@ namespace DSharpBot.Music_Player
 
         private VoiceNextConnection _connection;
         private List<Song> _playlist;
+        private ulong _interactionChannel;
 
         public bool IsPlaying { get; private set; }
-
+        public VoiceNextConnection Connection { get { return _connection; } }
+        
+        public MusicPlayer()
+        {
+            _playlist = new();
+            Program.discord.VoiceStateUpdated += CheckForAutoDisconnectAsync;
+        }
         public async Task ConnectAsync(InteractionContext ctx)
         {
             await ctx.Member.VoiceState.Channel.ConnectAsync();
 
             _connection = ctx.Client.GetVoiceNext().GetConnection(ctx.Guild);
+            _interactionChannel = ctx.Channel.Id;
         }
         public void Disconnect()
         {
@@ -37,17 +47,39 @@ namespace DSharpBot.Music_Player
             }
             return false;
         }
-        public void PlayThroughPlaylist()
+        public void PlayThroughPlaylist(InteractionContext ctx)
         {
             IsPlaying = true;
 
             while (_playlist.Count >= 1)
             {
-                
+
+                PlaySong(_playlist[0]);
+                _playlist.RemoveAt(0);
             }
 
             IsPlaying = false;
         }
-        
+        private void PlaySong(Song song)
+        {
+
+        }
+        private void DownloadSong(Song song)
+        {
+
+        }
+        private async Task CheckForAutoDisconnectAsync(DiscordClient client, VoiceStateUpdateEventArgs e)
+        {
+            if(e.User != Program.discord.CurrentUser && _connection.TargetChannel.Users.Count == 1)
+            {
+                Program.discord.VoiceStateUpdated -= CheckForAutoDisconnectAsync;
+
+                Disconnect();
+
+                await new DiscordMessageBuilder().WithContent("Bot disconnected due to lack of users in call").SendAsync(e.Guild.Channels[_interactionChannel]);
+
+                Players.Remove(e.Guild);
+            }
+        }
     }
 }
--- Music Player/MusicPlayerCommands.cs
+++ Music Player/MusicPlayerCommands.cs
@@ -1,7 +1,11 @@
-﻿using DSharpBot.Music_Player;
-using DSharpPlus;
+﻿using DSharpPlus;
 using DSharpPlus.SlashCommands;
+using DSharpPlus.VoiceNext;
+using Emzi0767.Utilities;
+using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using static DSharpBot.Music_Player.MusicPlayer;
 
 namespace DSharpBot.Music_Player
 {
@@ -10,37 +14,78 @@ namespace DSharpBot.Music_Player
         [SlashCommand("Join", "Adds the bot to the call.")]
         public async Task JoinCall(InteractionContext ctx)
         {
-            if (MusicPlayer.Players.ContainsKey(ctx.Guild))
+            if (Players.ContainsKey(ctx.Guild))
             {
                 ctx.CreateResponseAsync("The bot is already in a call!");
                 return;
             }
-            else
+            else if (ctx.Member.VoiceState == null)
             {
-                MusicPlayer.Players.Add(ctx.Guild, new());
-                await MusicPlayer.Players[ctx.Guild].ConnectAsync(ctx);
-                ctx.CreateResponseAsync("Joined call!");
+                ctx.CreateResponseAsync("You must be in a voice channel to have the bot join your call!");
+                return;
             }
+
+            Players.Add(ctx.Guild, new());
+            await Players[ctx.Guild].ConnectAsync(ctx);
+
+            ctx.CreateResponseAsync("Joined call!");
         }
 
         [SlashCommand("Disconnect", "Removes the bot from the call.")]
         public async Task Disconnect(InteractionContext ctx)
         {
-            if (!MusicPlayer.Players.ContainsKey(ctx.Guild))
+            if (!Players.ContainsKey(ctx.Guild))
             {
                 ctx.CreateResponseAsync("The bot is not in a call!");
+                return;
             }
-            else
+            else if(ctx.Member.VoiceState == null ||
+                ctx.Member.VoiceState.Channel != Players[ctx.Guild].Connection.TargetChannel)
             {
-                MusicPlayer.Players[ctx.Guild].Disconnect();
-                MusicPlayer.Players.Remove(ctx.Guild);
-                ctx.CreateResponseAsync("Disconnected from call!");
+                ctx.CreateResponseAsync("You cannot disconnect the bot if you are not in the same call as the bot!");
+                return;
             }
+
+            Players[ctx.Guild].Disconnect();
+            Players.Remove(ctx.Guild);
+
+            ctx.CreateResponseAsync("Disconnected from call!");
         }
+
         [SlashCommand("SongTest", "Song test")]
         public async Task SongTest(InteractionContext ctx, [Option("Song", "Url or search quary")] string input)
         {
-            ctx.CreateResponseAsync(new Song(input).ToString()));
+            ctx.CreateResponseAsync(new Song(input).ToString());
+        }
+        
+        [SlashCommand("Play", "Plays a song")]
+        public async Task PlaySong(InteractionContext ctx, [Option("Song", "Url or search quary")] string input)
+        {
+            if (!Players.ContainsKey(ctx.Guild))
+            {
+                ctx.CreateResponseAsync("The bot is not in a call!");
+            }
+            else if (ctx.Member.VoiceState == null ||
+                ctx.Member.VoiceState.Channel != Players[ctx.Guild].Connection.TargetChannel)
+            {
+                ctx.CreateResponseAsync("You must be in the same call as the bot in order to play a song through it!");
+                return;
+            }
+
+            Song song = new(input);
+
+            if (!Players[ctx.Guild].AddToPlaylist(song))
+            {
+                ctx.CreateResponseAsync($"Couldn't add {song.SongName} to the playlist because it was too long!");
+                return;
+            }
+
+            ctx.CreateResponseAsync($"Added {song} to the playlist!");
+
+            if (!Players[ctx.Guild].IsPlaying)
+            {
+                Players[ctx.Guild].PlayThroughPlaylist(ctx);
+            }
         }
     }
 }
